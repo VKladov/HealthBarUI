496450090ff2026c6385b3f007b9dd239f6e1cba
Vladimir <v.r.kladov@gmail.com>
2020-10-06 22:46:05 +0300

    added files

---
+++ Assets/Scripts/ChangeSliderValue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ChangeSliderValue : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Image _fillImage;
+    [SerializeField] private Color _fullColor;
+    [SerializeField] private Color _emptyColor;
+
+    public void ChangeValue(float delta)
+    {
+        float newValue = Mathf.Max(_slider.minValue, Mathf.Min(_slider.maxValue, _slider.value + delta));
+
+        _slider.DOValue(newValue, 0.3f).SetEase(Ease.OutBack);
+        _fillImage.DOColor(Color.Lerp(_emptyColor, _fullColor, (newValue - _slider.minValue) / (_slider.maxValue - _slider.minValue)), 0.3f);
+    }
+}
---
+++ Assets/Scripts/DeltaButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeltaButton : MonoBehaviour
+{
+    [SerializeField] private float _delta;
+    [SerializeField] private Text _buttonText;
+    [SerializeField] private ChangeSliderValue _changeSliderValue;
+
+    public void OnClicked()
+    {
+        _changeSliderValue.ChangeValue(_delta);
+    }
+
+    private void Awake()
+    {
+        _buttonText.text = (_delta > 0 ? "+" : "") + _delta;
+    }
+}
